dbc958ad9c5d72912de4f85eb8df761b50c609ac
topepo <mxkuhn@gmail.com>
2014-05-16 11:23:46 -0400

    File move from R-Forge `svn` document
    The are the files exposed by
    `http://desirability.r-forge.r-project.org/`.

---
+++ Site/The_desirability_Package_files/The_desirability_Package.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.3 local-build-20080604
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,426),url:'The_desirability_Package_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'The_desirability_Package_files/stroke_1.png'},{rect:new IWRect(1,-1,432,2),url:'The_desirability_Package_files/stroke_2.png'},{rect:new IWRect(433,-1,3,2),url:'The_desirability_Package_files/stroke_3.png'},{rect:new IWRect(433,1,3,426),url:'The_desirability_Package_files/stroke_4.png'},{rect:new IWRect(433,427,3,2),url:'The_desirability_Package_files/stroke_5.png'},{rect:new IWRect(1,427,432,2),url:'The_desirability_Package_files/stroke_6.png'},{rect:new IWRect(-1,427,2,2),url:'The_desirability_Package_files/stroke_7.png'}],new IWSize(434,428)),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('The_desirability_Package_files/The_desirability_PackageMoz.css')
+Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
